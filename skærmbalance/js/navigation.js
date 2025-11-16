e9d979126b9bbfd51bd80c29ea6a86974e56e43f
silkesiff <terlar01@iba.dk>
2025-11-16 16:30:48 +0700

    Færdiggjort side to forståelse.html

--- skærmbalance/js/navigation.js
+++ skærmbalance/js/navigation.js
@@ -7,3 +7,24 @@ const openNav = () => {
 }
 
 bars.addEventListener("click", openNav);
+
+/*Javascript til at lave accordion er taget direkte fra W3 school "How TO - Collapsibles/Accordion" og kopiret ind herunder*/
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
